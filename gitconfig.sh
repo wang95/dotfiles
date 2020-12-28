02b4293010abcb836bd034a9bbe7b23a061e2c77
wang95 <wang95@vip.qq.com>
2020-12-28 21:32:30 +0800

    add gitconfig

---
+++ gitconfig.sh
@@ -0,0 +1,12 @@
+git config --global user.name "wang95"
+git config --global user.email "wang95@vip.qq.com"
+git config --global color.ui true
+git config --global alias.st status
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.br branch
+git config --global alias.sw switch
+git config --global alias.unstage 'reset HEAD'
+git config --global alias.last 'log -1'
+git config --global alias.lg "log --color --graph --date=format:'%Y-%m-%d %H:%M' --pretty=tformat:'%C(red)%h %C(green)%cd%C(auto)%d%Creset %s %C(blue)<%an>%Creset' --abbrev-commit"
+# git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
